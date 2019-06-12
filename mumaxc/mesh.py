d4cd4b8e8fbdb25401ddc69c542f1d58976a2331
Marijan Beg <marijan.beg@xfel.eu>
2019-06-12 10:35:48 +0200

    Add system and mesh.

---
+++ mumaxc/mesh.py
@@ -0,0 +1,13 @@
+import textwrap
+import discretisedfield as df
+
+
+class Mesh(df.Mesh):
+    @property
+    def _script(self):
+        mx3 = "SetGridSize({}, {}, {})\n".format(self.n[0], self.n[1], self.n[2])
+        mx3 += "SetCellSize({}, {}, {})\n\n".format(self.cell[0],
+                                                    self.cell[1],
+                                                    self.cell[2])
+
+        return mx3
