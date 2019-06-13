03473af6d749ec75be755e7b560ff49b97d1f9f7
jmulkers <jeroen.mulkers@gmail.com>
2019-06-13 14:51:00 +0200

    enable DMI

--- mumaxc/hamiltonian/__init__.py
+++ mumaxc/hamiltonian/__init__.py
@@ -3,5 +3,5 @@ from .uniaxialanisotropy import UniaxialAnisotropy
 from .cubicanisotropy import CubicAnisotropy
 from .demag import Demag
 from .zeeman import Zeeman
-#from .dmi import DMI
+from .dmi import DMI
 from .hamiltonian import Hamiltonian
