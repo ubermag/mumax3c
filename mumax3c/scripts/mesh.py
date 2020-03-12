6ada1b8ff7082aeb30cc372649fcaf2367410a6c
Marijan Beg <m.beg@soton.ac.uk>
2020-03-12 13:25:18 +0000

    Mesh script.

---
+++ mumax3c/scripts/mesh.py
@@ -0,0 +1,17 @@
+import mumax3c as mc
+
+
+def mesh_script(mesh):
+    mx3 = ''
+    if mesh.pbc:
+        repetitions = [0, 0, 0]
+        for direction in mesh.pbc:
+            # I need to figure the way of setting up the repetitions.
+            repetitions[df.util.axesdict(direction)] = 1
+        mx3 += 'SetPBC({}, {}, {})\n'.format(*repetitions)
+    mx3 +=  'SetGridSize({}, {}, {})\n'.format(*mesh.n)
+    mx3 += 'SetCellSize({}, {}, {})\n\n'.format(*mesh.cell)
+
+    # Defining regions have to be added here.
+
+    return mx3
