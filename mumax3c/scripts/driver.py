9bb34ab9191c1b7c7bfb247c0fd2b3c53f2829c6
Marijan Beg <m.beg@soton.ac.uk>
2020-03-12 14:10:37 +0000

    Update.

---
+++ mumax3c/scripts/driver.py
@@ -0,0 +1,30 @@
+import oommfc as oc
+import micromagneticmodel as mm
+
+
+def driver_script(driver, system, compute=None, **kwargs):
+    mif = ''
+    if isinstance(driver, oc.MinDriver):
+        mx3 = ''
+
+        mx3 += f"Msat={system.m.norm.average}\n"
+
+        mx3 += "tableadd(E_total)\n"
+        mx3 += "tableadd(E_exch)\n"
+        mx3 += "tableadd(E_demag)\n"
+        mx3 += "tableadd(E_zeeman)\n"
+        mx3 += "tableadd(E_anis)\n"
+        mx3 += "tableadd(dt)\n"
+        mx3 += "tableadd(maxtorque)\n"
+
+
+        mx3 += "minimize()\n\n"
+        mx3 += "save(m_full)\n"
+        mx3 += "tablesave()\n\n"
+
+
+
+    if isinstance(driver, oc.TimeDriver):
+        pass
+
+    return mx3
