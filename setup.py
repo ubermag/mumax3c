5f2eccfe5251dca44c6c5d212c6f5dd0ae27427c
Marijan Beg <m.beg@soton.ac.uk>
2020-03-12 12:18:32 +0000

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='oommfc',
+    version='0.8.16',
+    description=('OOMMF calculator.'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://ubermag.github.io',
+    author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['micromagnetictests',
+                      'ubermagtable'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Science/Research',
+                 'Natural Language :: English']
+)
