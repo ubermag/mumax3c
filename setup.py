82ef8a2c53570b7e80c492b31b9dd5db275c87b1
Martin Lang <m.lang@soton.ac.uk>
2022-04-13 18:49:15 +0200

    Update repository metadata

--- setup.py
+++ setup.py
@@ -1,27 +1,4 @@
+# This content is required for editable install via pip.
 import setuptools
 
-with open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
-
-setuptools.setup(
-    name='mumax3c',
-    version='0.1',
-    description=('mumax3 calculator.'),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://ubermag.github.io',
-    author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    install_requires=['micromagnetictests',
-                      'ubermagtable'],
-    classifiers=['Development Status :: 3 - Alpha',
-                 'License :: OSI Approved :: BSD License',
-                 'Programming Language :: Python :: 3 :: Only',
-                 'Operating System :: Unix',
-                 'Operating System :: MacOS',
-                 'Operating System :: Microsoft :: Windows',
-                 'Topic :: Scientific/Engineering :: Physics',
-                 'Intended Audience :: Science/Research',
-                 'Natural Language :: English']
-)
+setuptools.setup()
