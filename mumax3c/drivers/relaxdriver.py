63d5872f1d8d73fb0bc256aaa3248158a853b76f
Kauser Zulfiqar <kauser.zulfiqar@mpsd.mpg.de>
2024-01-24 12:34:22 +0100

    updated the name of the check system funtion to _check_syatem

--- mumax3c/drivers/relaxdriver.py
+++ mumax3c/drivers/relaxdriver.py
@@ -49,7 +49,7 @@ class RelaxDriver(Driver):
     def _checkargs(self, **kwargs):
         pass  # no kwargs should be checked
 
-    def check_system(self, system):
+    def _check_system(self, system):
         """Checks the system has dynamics in it"""
         if len(system.dynamics) == 0:
             raise AttributeError("System's dynamics is not defined")
