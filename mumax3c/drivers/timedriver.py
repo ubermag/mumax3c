7f6b75f36c7afeeed2e1c748acfe5a2dce56064c
Sam Holt <48217392+samjrholt@users.noreply.github.com>
2024-03-01 12:43:55 +0100

    Change Error type

--- mumax3c/drivers/timedriver.py
+++ mumax3c/drivers/timedriver.py
@@ -65,7 +65,7 @@ class TimeDriver(Driver):
     def _check_system(self, system):
         """Checks the system has dynamics in it"""
         if len(system.dynamics) == 0:
-            raise AttributeError("System's dynamics is not defined")
+            raise RuntimeError("System's dynamics is not defined")
         if len(system.energy) == 0:
             raise AttributeError("System's energy is not defined")
 
