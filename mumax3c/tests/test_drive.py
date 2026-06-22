ae0ffd9301cbc2814908df12ff9d2a2137473b6c
Martin Lang <martin.lang@mpsd.mpg.de>
2026-06-22 12:30:40 +0200

    Add tests for micromagneticdata.Drive

--- mumax3c/tests/test_drive.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from micromagneticdata.testing.drive import *  # noqa: F403
-
-# Additional mumax3 specific tests
-
-
-@pytest.fixture
-def calculator_script_content():
-    return "tableadd"
