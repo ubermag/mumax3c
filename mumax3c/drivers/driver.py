e796e40ba759cb8b057ed3e60c0f378a61ef3e8d
Martin Lang <martin.lang@mpsd.mpg.de>
2026-09-02 15:56:14 +0200

    Update reading table after simulation has finished

--- mumax3c/drivers/driver.py
+++ mumax3c/drivers/driver.py
@@ -2,11 +2,11 @@ import abc
 import pathlib
 
 import discretisedfield as df
-import ubermagtable as ut
 import ubermagutil as uu
 from micromagneticmodel import adapter_base
 
 import mumax3c as mc
+import mumax3c.plugins
 
 
 class Driver(adapter_base.ExternalDriver):
@@ -163,7 +163,7 @@ class Driver(adapter_base.ExternalDriver):
         system.m.array = df.Field.from_file(str(lastovffile)).array
         system.m.norm = norm_field
 
-        system.table = ut.Table.fromfile(
+        system.table = mumax3c.plugins.table_from_file(
             str(pathlib.Path(f"{system.name}.out/table.txt")), x=self._x
         )
 
